e8dbb2354d360db2ac6664864e7db1e31190e721
Ben Spenard & Mike Rapp <student@epicodus.com>
2015-09-09 10:10:06 -0700

    make lowercase second spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 
-var pigLatin = function(text) {
-    var first = text.charAt(0);
-    if (first == "a" || "e" || "i" || "u")
+var pigLatin = function(string) {
+    var lowerString = string.toLowerCase();
+    var first = lowerString.charAt(0);
+    if (first == "a" || "e" || "i" || "u" || "o") //and sometimes"y"
     {
-        var result = (text + "ay");
+        var result = (lowerString + "ay");
     }
     console.log(result);
     return result;
 }
 
-$(document).ready(function() {
-  $("form#text").submit(function(event) {
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-
-    $(".year").text(year);
-    if(!result) {
-      $(".not").text("not");
-    } else {
-      $(".not").empty();
-    }
-
-    $("#result").show();
-    event.preventDefault();
-  });
-});
+// $(document).ready(function() {
+//   $("form#text").submit(function(event) {
+//     var year = parseInt($("input#year").val());
+//     var result = leapYear(year);
+//
+//     $(".year").text(year);
+//     if(!result) {
+//       $(".not").text("not");
+//     } else {
+//       $(".not").empty();
+//     }
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,9 @@ describe('pigLatin', function() {
     it("adds an 'ay' to the end of words that start with a vowel", function(){
     expect(pigLatin("apple")).to.equal("appleay");
     });
+
+    
+    it("handles mixed case entries", function(){
+        expect(pigLatin("ApPlE")).to.equal("appleay");
+    });
 });
